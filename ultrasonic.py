35383b984b2542cb855bbc1451658a4d63da3244
arpitvaghela <arpitvaghela9210@gmail.com>
2022-04-19 16:19:03 +0530

    working ultrasonic test

--- ultrasonic.py
+++ ultrasonic.py
@@ -1,40 +1,41 @@
-# Libraries
 import RPi.GPIO as GPIO
 import time
+import functools
 
 # GPIO Mode (BOARD / BCM)
 GPIO.setmode(GPIO.BCM)
 
 # set GPIO Pins
-GPIO_TRIGGER_1 = 20
-GPIO_ECHO_1 = 21
-GPIO_TRIGGER_2 = 17
-GPIO_ECHO_2 = 27
+GPIO_TRIGGER_FRONT = 20
+GPIO_ECHO_FRONT = 21
+
+GPIO_TRIGGER_SIDE = 27
+GPIO_ECHO_SIDE = 17
 
 # set GPIO direction (IN / OUT)
-GPIO.setup(GPIO_TRIGGER_1, GPIO.OUT)
-GPIO.setup(GPIO_ECHO_1, GPIO.IN)
-GPIO.setup(GPIO_TRIGGER_2, GPIO.OUT)
-GPIO.setup(GPIO_ECHO_2, GPIO.IN)
+GPIO.setup(GPIO_TRIGGER_FRONT, GPIO.OUT)
+GPIO.setup(GPIO_ECHO_FRONT, GPIO.IN)
+GPIO.setup(GPIO_TRIGGER_SIDE, GPIO.OUT)
+GPIO.setup(GPIO_ECHO_SIDE, GPIO.IN)
 
 
-def distance_1():
+def distance(GPIO_TRIGGER, GPIO_ECHO):
     # set Trigger to HIGH
-    GPIO.output(GPIO_TRIGGER_1, True)
+    GPIO.output(GPIO_TRIGGER, True)
 
     # set Trigger after 0.01ms to LOW
     time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER_1, False)
+    GPIO.output(GPIO_TRIGGER, False)
 
     StartTime = time.time()
     StopTime = time.time()
 
     # save StartTime
-    while GPIO.input(GPIO_ECHO_1) == 0:
+    while GPIO.input(GPIO_ECHO) == 0:
         StartTime = time.time()
 
     # save time of arrival
-    while GPIO.input(GPIO_ECHO_1) == 1:
+    while GPIO.input(GPIO_ECHO) == 1:
         StopTime = time.time()
 
     # time difference between start and arrival
@@ -46,43 +47,20 @@ def distance_1():
     return distance
 
 
-def distance_2():
-    # set Trigger to HIGH
-    GPIO.output(GPIO_TRIGGER_2, True)
+distance_front = functools.partial(distance, GPIO_TRIGGER_FRONT, GPIO_ECHO_FRONT)
+distance_side = functools.partial(distance, GPIO_TRIGGER_SIDE, GPIO_ECHO_SIDE)
 
-    # set Trigger after 0.01ms to LOW
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER_2, False)
-
-    StartTime = time.time()
-    StopTime = time.time()
-
-    # save StartTime
-    while GPIO.input(GPIO_ECHO_2) == 0:
-        StartTime = time.time()
-
-    # save time of arrival
-    while GPIO.input(GPIO_ECHO_2) == 1:
-        StopTime = time.time()
-
-    # time difference between start and arrival
-    TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = (TimeElapsed * 34300) / 2
-
-    return distance
-
-
-if __name__ == "_main_":
+if __name__ == "__main__":
     try:
         while True:
-            dist_1 = distance_1()
-            dist_2 = distance_2()
-            print("Measured Distance 1 = %.1f cm" % dist_1)
-            print("Measured Distance 2 = %.1f cm" % dist_2)
-            time.sleep(0.1)
-    # Reset by pressing CTRL + C
+            dist_1 = distance_front()
+            dist_2 = distance_side()
+            print("Measured Distance front = %.1f cm" % dist_1)
+            print("Measured Distance side = %.1f cm" % dist_2)
+
+            time.sleep(0.5)
+
+        # Reset by pressing CTRL + C
     except KeyboardInterrupt:
         print("Measurement stopped by User")
         GPIO.cleanup()
