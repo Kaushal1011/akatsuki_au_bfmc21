f2ad3589086745a2d648a7a914af3fa984d82d6e
kaushal1011 <kaushalpatil10@gmail.com>
2022-05-12 15:36:26 +0300

    lk testing

--- src/lib/cortex/decisionproc.py
+++ src/lib/cortex/decisionproc.py
@@ -388,8 +388,8 @@ class DecisionMakingProcess(WorkerProcess):
                 logger.debug(f"Sonar Side: {self.state.side_distance}")
 
                 if len(outPs) > 0:
-                    # outPs[0].send((self.state.steering_angle, self.state.v))
-                    outPs[0].send((0.0, 0.0))
+                    outPs[0].send((self.state.steering_angle, self.state.v))
+                    # outPs[0].send((0.0, 0.0))
                 # sleep(0.1)
 
             except Exception as e:
